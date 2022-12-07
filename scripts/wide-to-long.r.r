340b144cfd290981706deca2678446ec03c7a0a5
bautheac <o.bautheac@gmail.com>
2022-12-07 11:29:07 +0000

    updated dashboard with common stock data availability summary for 1913-first 50 firms sample

--- scripts/wide-to-long.r.r
+++
@@ -1,9 +0,0 @@
-library(magrittr)
-
-data_availability <- readr::read_csv("wide.csv")
-
-availables <- tidyr::pivot_longer(
-  data_availability, cols = c("traded", "price", "spread"), "field", "value"
-)
-readr::write_csv(availables, "long.csv")
-
