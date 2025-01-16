b8a9ea47aefffe2c7454bf01c3c1659b2939b926
bautheac <o.bautheac@gmail.com>
2025-01-16 16:27:27 +0000

    Make clean events dataset

--- computations/R/scripts/extract-transform-load/events/transformer/main.r
+++ computations/R/scripts/extract-transform-load/events/transformer/main.r
@@ -1,22 +1,23 @@
 pacman::p_load(here, modules)
 
 
-path_events_cleaner <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "globals", "transformer", "raw_price_data_names_cleaner.r")
-events_cleaner <- modules::use(path_events_cleaner)
+path_global_price_transformer <- 
+  here::here("computations", "R", "scripts", "extract-transform-load", "globals", "transformer", "prices", "main.r")
+global_price_transformer <- modules::use(path_global_price_transformer)
 path_variables_selecter <- 
   here::here("computations", "R", "scripts", "extract-transform-load", "events", "transformer", "variables_selecter.r")
 variables_selecter <- modules::use(path_variables_selecter)
-path_filter <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "globals", "filter", "main.r")
-filter <- modules::use(path_filter)
+path_variables_cleaner <- 
+  here::here("computations", "R", "scripts", "extract-transform-load", "events", "transformer", "variables_cleaner.r")
+variables_cleaner <- modules::use(path_variables_cleaner)
+
 
 
 export("transform_events")
 transform_events <- function(raw_events){
   
-  events_cleaner$rename_variables(raw_events) |>
+  global_price_transformer$transform_prices(raw_events) |>
     variables_selecter$select_relevant_variables() |>
-    filter$keep_distinct_variable_combinations(id, date, event) |>
+    variables_cleaner$clean_relevant_variables() |>
     dplyr::arrange(id, date, event)
 }
