b50034c0c663fae909810b5d2e526721ffab1471
bautheac <o.bautheac@gmail.com>
2025-01-17 11:18:13 +0000

    Correct bug in etl returns filter

--- computations/R/scripts/extract-transform-load/returns/transformer/filter.r
+++ computations/R/scripts/extract-transform-load/returns/transformer/filter.r
@@ -5,6 +5,9 @@ path_global_filter <-
 global_filter <- modules::use(path_global_filter)
 
 filter_out_first_row_of_every_group <- function(data, group_vars){
-  dplyr::group_by(data, !!!group_vars) |>
+  
+  group_vars_syms <- rlang::syms(group_vars)
+  
+  dplyr::group_by(data, !!!group_vars_syms) |>
     global_filter$filter_out_first_row()
 }
--- computations/R/scripts/extract-transform-load/returns/transformer/returns_computer.r
+++ computations/R/scripts/extract-transform-load/returns/transformer/returns_computer.r
@@ -1,4 +1,4 @@
-pacman::p_load(dplyr, modules)
+pacman::p_load(dplyr, modules, rlang)
 
 
 compute_mid_from_high_low_quotes <- function(high, low){
@@ -20,16 +20,18 @@ compute_returns_from_high_low_quotes <- function(high, low){
   return(returns)
 }
 
-compute_returns_by_group <- function(price_data, group_var, result_var){
+compute_returns_by_group <- function(price_data, group_vars, result_var){
 
-  dplyr::group_by(price_data, !!group_var) |>
+  group_vars_syms <- rlang::syms(group_vars)
+  
+  dplyr::group_by(price_data, !!!group_vars_syms) |>
     dplyr::mutate(!!result_var := compute_returns_from_high_low_quotes(high, low)) |>
     dplyr::ungroup()
 }
 
 compute_returns_by_company <- function(price_data){
 
-  compute_returns_by_group(price_data, group_var = "id", result_var = "return")
+  compute_returns_by_group(price_data, group_var = c("id"), result_var = "return")
 }
 
 export("compute_returns")
