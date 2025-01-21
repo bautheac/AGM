9628ab2becba9b9e2ed0b5f08084174339b83300
bautheac <o.bautheac@gmail.com>
2025-01-21 11:02:18 +0000

    Correct corrupt event records

--- computations/R/scripts/extract-transform-load/events/transformer/variables_cleaner.r
+++ computations/R/scripts/extract-transform-load/events/transformer/variables_cleaner.r
@@ -1,7 +1,20 @@
 pacman::p_load(dplyr, modules)
 
+clean_corrupts_event_records <- function(events_data){
+  
+  dplyr::mutate(
+    events_data,
+    event = ifelse(id == "BA031" & date == "1913-07-25", "", event),
+    event = ifelse(id == "BA031" & date == "1913-07-26", "", event),
+    event = ifelse(id == "BA031" & date == "1913-07-27", "", event),
+    event = ifelse(id == "BA031" & date == "1913-07-28", "", event),
+    event = ifelse(id == "BA031" & date == "1913-07-29", "", event),
+  )
+}
+
 modules::export("clean_relevant_variables")
 clean_relevant_variables <- function(events_data){
   
-  dplyr::filter(events_data, !(is.na(event) | event == ""))
+  clean_corrupts_event_records(events_data) |>
+    dplyr::filter(!(is.na(event) | event == ""))
 }
