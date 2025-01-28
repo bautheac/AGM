36a68601aa4e47956507351ae5f71ebf85799be0
bautheac <o.bautheac@gmail.com>
2025-01-28 11:31:04 +0000

    Refactoring
    returns descriptive statistics main

--- computations/R/scripts/analysis/descriptive-statistics/returns/main.r
+++ computations/R/scripts/analysis/descriptive-statistics/returns/main.r
@@ -11,8 +11,9 @@ path_staistics_computer <- here::here(
   "statistics-computer", "main.r"
 )
 statistics_computer <- modules::use(path_staistics_computer)
-path_data_loader <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "globals", "loader.r")
+path_data_loader <- here::here(
+  "computations", "R", "scripts", "extract-transform-load", "globals", "loader.r"
+)
 data_loader <- modules::use(path_data_loader)
 path_globals <- here::here(
   "computations", "R", "scripts", "analysis", "descriptive-statistics", "returns", 
@@ -27,12 +28,18 @@ dataset <- wrangler$make_dataset()
 statistics <- statistics_computer$compute_returns_statistics(dataset)
 
 
+paths_firm <- list(
+  globals$path_returns_firms_descriptive_stats_results, 
+  globals$path_returns_firms_descriptive_stats_dashboard
+)
+paths_aggregate <- list(
+  globals$path_returns_aggregate_descriptive_stats_results, 
+  globals$path_returns_aggregate_descriptive_stats_dashboard
+)
+
 data_loader$load_objects(
   list(statistics$firm, statistics$aggregate), 
-  list(
-    list(globals$path_returns_firms_descriptive_stats_results, globals$path_returns_firms_descriptive_stats_dashboard),
-    list(globals$path_returns_aggregate_descriptive_stats_results, globals$path_returns_aggregate_descriptive_stats_dashboard)
-    )
+  list(paths_firm, paths_aggregate)
 )
 
 
