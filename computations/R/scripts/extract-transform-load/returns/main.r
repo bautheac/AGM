07cc64b6b497b336a56f9a25fdbe960ae7e8ffed
bautheac <o.bautheac@gmail.com>
2025-02-03 09:54:18 +0000

    Refactoring
    Move operations to transformer + introduce wrangler to round results to 4 decimals

--- computations/R/scripts/extract-transform-load/returns/main.r
+++ computations/R/scripts/extract-transform-load/returns/main.r
@@ -5,31 +5,29 @@ path_price_data_globals <- here::here(
   "computations", "R", "scripts", "extract-transform-load", "prices", "globals.r"
 )
 price_data_globals <- modules::use(path_price_data_globals)
-path_data_extracter <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "globals", "extracter.r")
+path_data_extracter <- here::here(
+  "computations", "R", "scripts", "extract-transform-load", "globals", "extracter.r"
+)
 data_extracter <- modules::use(path_data_extracter)
-path_variables_selecter <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "returns", "transformer", "variables_selecter.r")
-variables_selecter <- modules::use(path_variables_selecter)
-path_returns_computer <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "returns", "transformer", "returns_computer.r")
-returns_computer <- modules::use(path_returns_computer)
-path_filter <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "returns", "transformer", "filter.r")
-filter <- modules::use(path_filter)
-path_data_loader <- 
-  here::here("computations", "R", "scripts", "extract-transform-load", "globals", "loader.r")
+path_transformer <- here::here(
+  "computations", "R", "scripts", "extract-transform-load", "returns", "transformer", 
+  "main.r"
+)
+transformer <- modules::use(path_transformer)
+path_data_loader <- here::here(
+  "computations", "R", "scripts", "extract-transform-load", "globals", "loader.r"
+)
 data_loader <- modules::use(path_data_loader)
-path_globals <- here::here("computations", "R", "scripts", "extract-transform-load", "returns", "globals.r")
+path_globals <- here::here(
+  "computations", "R", "scripts", "extract-transform-load", "returns", "globals.r"
+)
 globals <- modules::use(path_globals)
 
 
 price_data <- data_extracter$extract_rds_data( price_data_globals$filepath_clean_prices_data_main)
 
 
-clean_returns <- returns_computer$compute_returns(price_data) |>
-  variables_selecter$select_relevant_variables() |>
-  filter$filter_out_first_row_of_every_group("id")
+clean_returns <- transformer$make_returns_dataset(price_data)
 
 
 data_loader$load_objects(
