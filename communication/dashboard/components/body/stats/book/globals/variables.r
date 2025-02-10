9079d301c936a4e7b92b809b0582c4a97ab47936
bautheac <o.bautheac@gmail.com>
2025-02-10 15:48:47 +0000

    Correct typos

--- communication/dashboard/components/body/stats/book/globals/variables.r
+++ communication/dashboard/components/body/stats/book/globals/variables.r
@@ -1,11 +1,11 @@
 modules::export("caption_book")
 caption_book <- glue::glue("
-This page shows descriptive statistics for the book dataset. The results are shown 
-by reporting frequency starting with the stattistics for the firms reporting annually,
-followed by those for the firms reporting semi-annually, and finally the statistics 
-for all firms combined. For each year the statistics are shown for each reporting 
-period. Results for annually reporting firms naturally show only 1 reporting period, 
-while those for semi-annually reporting firms show 2 reporting periods. The 'n' 
-statistic documents the number of available observations for the correpsonding 
+This page shows descriptive statistics for the book dataset. The results are shown
+by reporting frequency starting with the statistics for the firms reporting annually,
+followed by those for the firms reporting semi-annually, and finally the statistics
+for all firms combined. For each year the statistics are shown for each reporting
+period. Results for annually reporting firms naturally show only 1 reporting period,
+while those for semi-annually reporting firms show 2 reporting periods. The 'n'
+statistic documents the number of available observations for the correpsonding
 variable-year-reporting period combination.
 ")
--- communication/dashboard/components/body/stats/returns/globals/variables.r
+++ communication/dashboard/components/body/stats/returns/globals/variables.r
@@ -1,14 +1,14 @@
 modules::export("caption_returns")
 caption_returns <- glue::glue("
 This page shows descriptive statistics for the returns dataset. The results are shown 
-by reporting frequency starting with the stattistics for the firms reporting annually,
+by reporting frequency starting with the statistics for the firms reporting annually,
 followed by those for the firms reporting semi-annually, and finally the statistics 
 for all firms combined. For each year the statistics are shown for each reporting 
 period. Results for annually reporting firms naturally show only 1 reporting period, 
 while those for semi-annually reporting firms show 2 reporting periods.<br>
 The '# days available' family of statistics refers to the number of return days and 
 non-zero return days available around corporate events. In this context, availability 
-in defined as no more that 5 successive missing records.  
+is defined as no more that 5 successive missing records.  
 ")
 
 modules::export("caption_firms")
